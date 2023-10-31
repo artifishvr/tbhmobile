ad9d4037455634e18595baf4312212b3afc06d6a
artificialbutter <59352535+artificialbutter@users.noreply.github.com>
2023-10-30 18:32:36 -0700

    haptics

--- src/tbh.js
+++ src/tbh.js
@@ -1,11 +1,17 @@
 import confetti from 'canvas-confetti';
 import audioURL from './yippee.mp3'
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
+
+
+const haptics = async () => {
+    await Haptics.impact({ style: ImpactStyle.Heavy });
+};
 
 function yippee() {
     for (let i = 0; i < 50; i++) {
         confetti({
             particleCount: 10,
-            spread: 50,            
+            spread: 50,
             startVelocity: 100,
             decay: 0.8,
             origin: { y: 1.2 },
@@ -20,7 +26,7 @@ function yippee() {
         });
         confetti({
             particleCount: 10,
-            spread: 50,            
+            spread: 50,
             angle: 120,
             startVelocity: 100,
             decay: 0.8,
@@ -32,6 +38,7 @@ function yippee() {
     audio.src = audioURL
     document.body.appendChild(audio);
     audio.play();
+    haptics();
 
     audio.onended = function () {
         this.parentNode.removeChild(this);
